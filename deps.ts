bcc52b66980d639e6f262415a35a24d8d004a035
Miguel Ripoll <miguel.ripoll@outlook.com>
2022-09-04 00:25:56 +0200

    Update std to v0.154.0 (#14)

--- deps.ts
+++ deps.ts
@@ -5,4 +5,4 @@ export { Select } from "https://deno.land/x/cliffy@v0.25.0/prompt/select.ts";
 export { Input } from "https://deno.land/x/cliffy@v0.25.0/prompt/mod.ts";
 export { Confirm } from "https://deno.land/x/cliffy@v0.25.0/prompt/confirm.ts";
 
-export * as semver from "https://deno.land/std@0.153.0/semver/mod.ts";
+export * as semver from "https://deno.land/std@0.154.0/semver/mod.ts";
