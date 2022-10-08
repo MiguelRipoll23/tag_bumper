de394aea0d8dc25332eaf406cc10ba1b36c99624
Miguel Ripoll <miguel.ripoll@outlook.com>
2022-10-08 12:27:31 +0200

    Update dependencies (#15)
    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-export type { SelectValueOptions } from "https://deno.land/x/cliffy@v0.25.0/prompt/select.ts";
+export type { SelectValueOptions } from "https://deno.land/x/cliffy@v0.25.2/prompt/select.ts";
 
-export { colors } from "https://deno.land/x/cliffy@v0.25.0/ansi/colors.ts";
-export { Select } from "https://deno.land/x/cliffy@v0.25.0/prompt/select.ts";
-export { Input } from "https://deno.land/x/cliffy@v0.25.0/prompt/mod.ts";
-export { Confirm } from "https://deno.land/x/cliffy@v0.25.0/prompt/confirm.ts";
+export { colors } from "https://deno.land/x/cliffy@v0.25.2/ansi/colors.ts";
+export { Select } from "https://deno.land/x/cliffy@v0.25.2/prompt/select.ts";
+export { Input } from "https://deno.land/x/cliffy@v0.25.2/prompt/mod.ts";
+export { Confirm } from "https://deno.land/x/cliffy@v0.25.2/prompt/confirm.ts";
 
-export * as semver from "https://deno.land/std@0.154.0/semver/mod.ts";
+export * as semver from "https://deno.land/std@0.159.0/semver/mod.ts";
