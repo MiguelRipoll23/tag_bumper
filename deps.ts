0a9269eddd7603fb8ca7a1cdd54621aef4ed0f97
Miguel Ripoll <miguel.ripoll@outlook.com>
2022-12-31 18:09:26 +0100

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-export type { SelectValueOptions } from "https://deno.land/x/cliffy@v0.25.4/prompt/select.ts";
+export type { SelectValueOptions } from "https://deno.land/x/cliffy@v0.25.6/prompt/select.ts";
 
-export { colors } from "https://deno.land/x/cliffy@v0.25.4/ansi/colors.ts";
-export { Select } from "https://deno.land/x/cliffy@v0.25.4/prompt/select.ts";
-export { Input } from "https://deno.land/x/cliffy@v0.25.4/prompt/mod.ts";
-export { Confirm } from "https://deno.land/x/cliffy@v0.25.4/prompt/confirm.ts";
+export { colors } from "https://deno.land/x/cliffy@v0.25.6/ansi/colors.ts";
+export { Select } from "https://deno.land/x/cliffy@v0.25.6/prompt/select.ts";
+export { Input } from "https://deno.land/x/cliffy@v0.25.6/prompt/mod.ts";
+export { Confirm } from "https://deno.land/x/cliffy@v0.25.6/prompt/confirm.ts";
 
-export * as semver from "https://deno.land/std@0.166.0/semver/mod.ts";
+export * as semver from "https://deno.land/std@0.170.0/semver/mod.ts";
